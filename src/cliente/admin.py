0722adde22f7ac17c1827c4f6622ad40abe448b1
Elias De Nadai Nalesso Silva <ps.eliasn@aguiabranca.com.br>
2025-12-04 06:27:21 -0300

    feat: update models

--- src/cliente/admin.py
+++ src/cliente/admin.py
@@ -1,6 +1,20 @@
 from django.contrib import admin
 
-from .models import Cliente
+from .models import (
+    Cliente,
+    Entrega,
+    Entregador,
+    Farmacia,
+    ItemPedido,
+    Medicamento,
+    Pedido,
+)
 
 
 admin.site.register(Cliente)
+admin.site.register(Entrega)
+admin.site.register(Entregador)
+admin.site.register(Farmacia)
+admin.site.register(ItemPedido)
+admin.site.register(Medicamento)
+admin.site.register(Pedido)
--- src/cliente/models.py
+++ src/cliente/models.py
@@ -4,11 +4,128 @@ from utils.validators import validate_cpf, validate_digits
 
 
 class Cliente(models.Model):
-    nome: models.CharField = models.CharField(max_length=20)
-    sobrenome: models.CharField = models.CharField(max_length=100)
-    email: models.EmailField = models.EmailField()
-    cpf: models.CharField = models.CharField(max_length=11, unique=True, validators=[validate_cpf], verbose_name='CPF')
-    telefone: models.CharField = models.CharField(max_length=11, unique=True, validators=[validate_digits])
+    nome: models.CharField = models.CharField(max_length=50)
+    sobrenome: models.CharField = models.CharField(blank=True, null=True, max_length=100)
+    email: models.EmailField = models.EmailField(unique=True)
+    cpf: models.CharField = models.CharField(
+        blank=True, null=True,
+        max_length=11,
+        unique=True,
+        validators=[validate_cpf],
+        verbose_name='CPF',
+    )
+    telefone: models.CharField = models.CharField(
+        blank=True, null=True,
+        max_length=11,
+        unique=True,
+        validators=[validate_digits],
+    )
 
     def __str__(self):
-        return f'#{self.pk}, {self.nome} <{self.email}>'
+        return f'#{self.pk}, {self.nome} {self.sobrenome if self.sobrenome else ""} <{self.email}>'
+
+
+class Farmacia(models.Model):
+    nome: models.CharField = models.CharField(max_length=50)
+    geo_loc: models.CharField = models.CharField(max_length=50)
+
+    def __str__(self):
+        return f'#{self.pk}, [{self.geo_loc}], {self.nome}'
+
+
+class Medicamento(models.Model):
+    nome: models.CharField = models.CharField(max_length=50)
+    qtd_estoque: models.PositiveIntegerField = models.PositiveIntegerField(default=0, verbose_name='Qtd. em Estoque')
+    farmacia: models.ForeignKey = models.ForeignKey(Farmacia, on_delete=models.PROTECT)
+    preco: models.DecimalField = models.DecimalField(max_digits=10, decimal_places=2)
+
+    def __str__(self):
+        return f'#{self.pk}, {self.qtd_estoque}x {self.nome}'
+
+
+class Entregador(models.Model):
+    nome: models.CharField = models.CharField(max_length=50)
+    sobrenome: models.CharField = models.CharField(
+        blank=True, null=True,
+        max_length=100,
+    )
+    email: models.EmailField = models.EmailField(unique=True)
+    cpf: models.CharField = models.CharField(
+        blank=True, null=True,
+        unique=True,
+        max_length=11,
+        validators=[validate_cpf],
+        verbose_name='CPF',
+    )
+    telefone: models.CharField = models.CharField(
+        max_length=11,
+        unique=True,
+        validators=[validate_digits],
+    )
+
+    def __str__(self):
+        return f'#{self.pk}, {self.nome} {self.sobrenome if self.sobrenome else ""} <{self.email}> <{self.telefone}>'
+
+    class Meta:
+        verbose_name_plural = 'Entregadores'
+
+
+class Entrega(models.Model):
+    entregador: models.OneToOneField = models.OneToOneField(
+        Entregador,
+        on_delete=models.PROTECT,
+    )
+    real_time_geo_loc_key: models.CharField = models.CharField(
+        blank=True, null=True,
+        max_length=50,
+    )
+
+    def __str__(self):
+        return f'#{self.pk}, [{self.real_time_geo_loc_key}], [Entregador: {self.entregador}]'
+
+
+class Pedido(models.Model):
+    class Status(models.IntegerChoices):
+        CLIENTE_ESCOLHENDO = 0, 'Cliente escolhendo'
+        SOLICITADO = 1, 'Solicitado'
+        SENDO_SEPARADO = 2, 'Sendo separado'
+        A_CAMINHO = 3, 'A caminho'
+        ENTREGUE = 4, 'Entregue'
+
+    status: models.PositiveSmallIntegerField = models.PositiveSmallIntegerField(choices=Status.choices)
+    data: models.DateTimeField = models.DateTimeField(auto_now_add=True)
+    cliente: models.ForeignKey = models.ForeignKey(Cliente, on_delete=models.PROTECT)
+    entrega: models.ForeignKey = models.ForeignKey(Entrega, on_delete=models.PROTECT, blank=True, null=True)
+
+    @property
+    def status_label(self):
+        return Pedido.Status(self.status).label
+
+    def __str__(self):
+        return f'#{self.pk}, [Status: {self.status_label}], [Cliente: {self.cliente}], [Entrega: {self.entrega}]'
+
+
+class ItemPedido(models.Model):
+    pedido: models.ForeignKey = models.ForeignKey(
+        Pedido,
+        related_name='itens',
+        on_delete=models.CASCADE,
+    )
+    medicamento: models.ForeignKey = models.ForeignKey(
+        Medicamento,
+        on_delete=models.DO_NOTHING,
+    )
+    quantidade: models.PositiveIntegerField = models.PositiveIntegerField(default=1)
+    preco_unitario: models.DecimalField = models.DecimalField(max_digits=10, decimal_places=2)
+
+    @property
+    def sub_total(self):
+        return self.quantidade * self.preco_unitario
+
+    def __str__(self):
+        return f'{self.quantidade}x {self.medicamento.nome}, [Pedido: {self.pedido}]'
+
+    class Meta:
+        unique_together = ('pedido', 'medicamento')
+        verbose_name = 'Item do Pedido'
+        verbose_name_plural = 'Itens do Pedido'
